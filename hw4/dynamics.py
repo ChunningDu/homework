4a5df28211c82153f097edc7c5764a573f9b3c22
btaba <btabanpour@gmail.com>
2017-10-21 17:22:41 -0400

    hw4

--- hw4/dynamics.py
+++ hw4/dynamics.py
@@ -1,15 +1,18 @@
 import tensorflow as tf
 import numpy as np
 
-# Predefined function to build a feedforward neural network
-def build_mlp(input_placeholder, 
+EPS = np.finfo(np.float32).eps
+
+
+def build_mlp(input_placeholder,
               output_size,
-              scope, 
-              n_layers=2, 
-              size=500, 
+              scope,
+              n_layers=2,
+              size=500,
               activation=tf.tanh,
               output_activation=None
               ):
+    # Predefined function to build a feedforward neural network
     out = input_placeholder
     with tf.variable_scope(scope):
         for _ in range(n_layers):
@@ -17,13 +20,14 @@ def build_mlp(input_placeholder,
         out = tf.layers.dense(out, output_size, activation=output_activation)
     return out
 
+
 class NNDynamicsModel():
-    def __init__(self, 
-                 env, 
+    def __init__(self,
+                 env,
                  n_layers,
-                 size, 
-                 activation, 
-                 output_activation, 
+                 size,
+                 activation,
+                 output_activation,
                  normalization,
                  batch_size,
                  iterations,
@@ -32,14 +36,95 @@ class NNDynamicsModel():
                  ):
         """ YOUR CODE HERE """
         """ Note: Be careful about normalization """
+        self._sess = sess
+        self._normalization = normalization
+        self._batch_size = batch_size
+        self._iterations = iterations
+        self._learning_rate = learning_rate
+
+        # assuming continuous obs and actions
+        obs_dim = env.observation_space.shape[0]
+        # obs_dim = 1 if len(obs_dim) == 0 else obs_dim[0]
+        ac_dim = env.action_space.shape[0]
+        # ac_dim = 1 if len(ac_dim) == 0 else ac_dim[0]
+        self._input_obs = tf.placeholder(
+            tf.float32, shape=[None, obs_dim])
+        self._input_acs = tf.placeholder(
+            tf.float32, shape=[None, ac_dim])
+        self._input = tf.concat(
+            [self._input_obs, self._input_acs], axis=1)
+        self._delta = tf.placeholder(
+            tf.float32, shape=[None, obs_dim])
+        self.network = build_mlp(
+            self._input, output_size=obs_dim,
+            scope='dynamics', n_layers=n_layers, size=size,
+            activation=activation, output_activation=output_activation)
+
+        self.loss = tf.reduce_sum(tf.reduce_mean(
+            tf.square(self.network - self._delta), axis=0))
+        self.optimizer = tf.train.AdamOptimizer(
+            learning_rate).minimize(self.loss)
 
     def fit(self, data):
         """
-        Write a function to take in a dataset of (unnormalized)states, (unnormalized)actions, (unnormalized)next_states and fit the dynamics model going from normalized states, normalized actions to normalized state differences (s_t+1 - s_t)
+        Write a function to take in a dataset of (unnormalized)states,
+        (unnormalized)actions, (unnormalized)next_states and fit the
+        dynamics model going from normalized states, normalized actions
+        to normalized state differences (s_t+1 - s_t)
         """
+        # self._normalization takes the following form
+        # mean_obs, std_obs, mean_deltas, std_deltas, mean_action, std_action
+        obs = np.concatenate(
+            list(map(lambda x: x['observations'], data)))
+        acs = np.concatenate(
+            list(map(lambda x: x['actions'], data)))
+        next_obs = np.concatenate(
+            list(map(lambda x: x['next_observations'], data)))
 
-        """YOUR CODE HERE """
+        norm_states = (obs - self._normalization[0]) /\
+            (self._normalization[1] + EPS)
+        deltas = obs - next_obs
+        norm_deltas = (deltas - self._normalization[2]) /\
+            (self._normalization[3] + EPS)
+        norm_actions = (acs - self._normalization[4]) /\
+            (self._normalization[5] + EPS)
+
+        for _ in range(self._iterations):
+            dataset = tf.data.Dataset.from_tensor_slices(
+                (norm_states, norm_actions, norm_deltas))
+            batch_dataset = dataset.shuffle(
+                    norm_states.shape[0]
+                ).batch(self._batch_size)
+            iterator = batch_dataset.make_one_shot_iterator()
+            next_element = iterator.get_next()
+            while True:
+                try:
+                    o, a, d = self._sess.run(next_element)
+                except tf.errors.OutOfRangeError:
+                    break
+                feed_dict = {
+                    self._input_obs: o,
+                    self._input_acs: a,
+                    self._delta: d
+                }
+                self._sess.run(self.optimizer, feed_dict)
 
     def predict(self, states, actions):
-        """ Write a function to take in a batch of (unnormalized) states and (unnormalized) actions and return the (unnormalized) next states as predicted by using the model """
-        """ YOUR CODE HERE """
+        """
+        Write a function to take in a batch of (unnormalized) states
+        and (unnormalized) actions and return the (unnormalized) next
+        states as predicted by using the model
+        """
+        norm_states = (states - self._normalization[0]) /\
+            (self._normalization[1] + EPS)
+        norm_actions = (actions - self._normalization[4]) /\
+            (self._normalization[5] + EPS)
+
+        feed_dict = {
+            self._input_obs: norm_states,
+            self._input_acs: norm_actions,
+        }
+        norm_delta = self._sess.run(self.network, feed_dict)
+        next_states = (norm_delta * self._normalization[3]) +\
+            self._normalization[2] + states
+        return next_states
--- hw4/main.py
+++ hw4/main.py
@@ -7,66 +7,107 @@ from cost_functions import cheetah_cost_fn, trajectory_cost_fn
 import time
 import logz
 import os
-import copy
 import matplotlib.pyplot as plt
-from cheetah_env import HalfCheetahEnvNew
+# from cheetah_env import HalfCheetahEnvNew
 
-def sample(env, 
-           controller, 
-           num_paths=10, 
-           horizon=1000, 
+
+def sample(env,
+           controller,
+           num_paths=10,
+           horizon=1000,
            render=False,
            verbose=False):
     """
-        Write a sampler function which takes in an environment, a controller (either random or the MPC controller), 
-        and returns rollouts by running on the env. 
-        Each path can have elements for observations, next_observations, rewards, returns, actions, etc.
+        Write a sampler function which takes in an environment,
+        a controller (either random or the MPC controller),
+        and returns rollouts by running on the env.
+        Each path can have elements for observations, next_observations,
+        rewards, returns, actions, etc.
     """
     paths = []
-    """ YOUR CODE HERE """
+
+    for n in range(num_paths):
+        print('Sampled {} out of {} paths'.format(n, num_paths))
+        path = {"observations": [], "rewards": [],
+                "next_observations": [], "actions": []}
+
+        t = 0
+        ob = env.reset()
+        while t <= horizon:
+            if t % 100 == 0 and t:
+                print('    Time step {} with horizon {}'.format(t, horizon))
+            a = controller.get_action(ob)
+            nob, rew, done, info = env.step(a)
+
+            path['observations'].append(ob)
+            path['rewards'].append(rew)
+            path['next_observations'].append(nob)
+            path['actions'].append(a)
+            ob = nob
+            t += 1
+            if done:
+                break
+
+        paths.append(path)
 
     return paths
 
-# Utility to compute cost a path for a given cost function
+
 def path_cost(cost_fn, path):
-    return trajectory_cost_fn(cost_fn, path['observations'], path['actions'], path['next_observations'])
+    # Utility to compute cost a path for a given cost function
+    return trajectory_cost_fn(
+        cost_fn, path['observations'], path['actions'],
+        path['next_observations'])
+
 
 def compute_normalization(data):
     """
     Write a function to take in a dataset and compute the means, and stds.
-    Return 6 elements: mean of s_t, std of s_t, mean of (s_t+1 - s_t), std of (s_t+1 - s_t), mean of actions, std of actions
+    Return 6 elements: mean of s_t, std of s_t, mean of (s_t+1 - s_t),
+    std of (s_t+1 - s_t), mean of actions, std of actions
     """
+    observations = np.concatenate([d['observations'] for d in data])
+    next_observations = np.concatenate([d['next_observations'] for d in data])
+    actions = np.concatenate([d['actions'] for d in data])
 
-    """ YOUR CODE HERE """
+    mean_obs = np.mean(observations, axis=0)
+    std_obs = np.std(observations, axis=0)
+    deltas = next_observations - observations
+    mean_deltas = np.mean(deltas, axis=0)
+    std_deltas = np.std(deltas, axis=0)
+    mean_action = np.mean(actions, axis=0)
+    std_action = np.std(actions, axis=0)
+
     return mean_obs, std_obs, mean_deltas, std_deltas, mean_action, std_action
 
 
 def plot_comparison(env, dyn_model):
     """
-    Write a function to generate plots comparing the behavior of the model predictions for each element of the state to the actual ground truth, using randomly sampled actions. 
+    Write a function to generate plots comparing the behavior of the
+    model predictions for each element of the state to the actual ground truth,
+    using randomly sampled actions.
     """
-    """ YOUR CODE HERE """
+    # paths = sample(env, dyn_model)
     pass
 
-def train(env, 
-         cost_fn,
-         logdir=None,
-         render=False,
-         learning_rate=1e-3,
-         onpol_iters=10,
-         dynamics_iters=60,
-         batch_size=512,
-         num_paths_random=10, 
-         num_paths_onpol=10, 
-         num_simulated_paths=10000,
-         env_horizon=1000, 
-         mpc_horizon=15,
-         n_layers=2,
-         size=500,
-         activation=tf.nn.relu,
-         output_activation=None
-         ):
 
+def train(env,
+          cost_fn,
+          logdir=None,
+          render=False,
+          learning_rate=1e-3,
+          onpol_iters=10,
+          dynamics_iters=60,
+          batch_size=512,
+          num_paths_random=10,
+          num_paths_onpol=10,
+          num_simulated_paths=10000,
+          env_horizon=1000,
+          mpc_horizon=15,
+          n_layers=2,
+          size=500,
+          activation=tf.nn.relu,
+          output_activation=None):
     """
 
     Arguments:
@@ -103,85 +144,95 @@ def train(env,
 
     logz.configure_output_dir(logdir)
 
-    #========================================================
-    # 
+    # ========================================================
+    #
     # First, we need a lot of data generated by a random
     # agent, with which we'll begin to train our dynamics
     # model.
 
     random_controller = RandomController(env)
 
-    """ YOUR CODE HERE """
+    data = sample(env, random_controller,
+                  num_paths_random, env_horizon)
 
-
-    #========================================================
-    # 
+    # ========================================================
+    #
     # The random data will be used to get statistics (mean
     # and std) for the observations, actions, and deltas
     # (where deltas are o_{t+1} - o_t). These will be used
     # for normalizing inputs and denormalizing outputs
-    # from the dynamics network. 
-    # 
-    normalization = """ YOUR CODE HERE """
+    # from the dynamics network.
+    #
+    normalization = compute_normalization(data)
 
-
-    #========================================================
-    # 
+    # ========================================================
+    #
     # Build dynamics model and MPC controllers.
-    # 
+    #
     sess = tf.Session()
 
-    dyn_model = NNDynamicsModel(env=env, 
-                                n_layers=n_layers, 
-                                size=size, 
-                                activation=activation, 
-                                output_activation=output_activation, 
+    dyn_model = NNDynamicsModel(env=env,
+                                n_layers=n_layers,
+                                size=size,
+                                activation=activation,
+                                output_activation=output_activation,
                                 normalization=normalization,
                                 batch_size=batch_size,
                                 iterations=dynamics_iters,
                                 learning_rate=learning_rate,
                                 sess=sess)
 
-    mpc_controller = MPCcontroller(env=env, 
-                                   dyn_model=dyn_model, 
-                                   horizon=mpc_horizon, 
-                                   cost_fn=cost_fn, 
+    mpc_controller = MPCcontroller(env=env,
+                                   dyn_model=dyn_model,
+                                   horizon=mpc_horizon,
+                                   cost_fn=cost_fn,
                                    num_simulated_paths=num_simulated_paths)
 
-
-    #========================================================
-    # 
+    # ========================================================
+    #
     # Tensorflow session building.
-    # 
+    #
     sess.__enter__()
     tf.global_variables_initializer().run()
 
-    #========================================================
-    # 
-    # Take multiple iterations of onpolicy aggregation at each iteration refitting the dynamics model to current dataset and then taking onpolicy samples and aggregating to the dataset. 
-    # Note: You don't need to use a mixing ratio in this assignment for new and old data as described in https://arxiv.org/abs/1708.02596
-    # 
+    # ========================================================
+    #
+    # Take multiple iterations of onpolicy aggregation at each iteration
+    # refitting the dynamics model to current dataset and then taking onpolicy
+    # samples and aggregating to the dataset.
+    # Note: You don't need to use a mixing ratio in this assignment for
+    # new and old data as described in https://arxiv.org/abs/1708.02596
+    #
     for itr in range(onpol_iters):
-        """ YOUR CODE HERE """
+        dyn_model.fit(data)
 
+        data_sample = sample(
+            env, mpc_controller,
+            num_paths=num_paths_onpol,
+            horizon=env_horizon)
 
+        data.append(data_sample)
 
         # LOGGING
         # Statistics for performance of MPC policy using
         # our learned dynamics model
         logz.log_tabular('Iteration', itr)
         # In terms of cost function which your MPC controller uses to plan
+        costs = [path_cost(cost_fn, ds) for ds in data_sample]
         logz.log_tabular('AverageCost', np.mean(costs))
         logz.log_tabular('StdCost', np.std(costs))
         logz.log_tabular('MinimumCost', np.min(costs))
         logz.log_tabular('MaximumCost', np.max(costs))
-        # In terms of true environment reward of your rolled out trajectory using the MPC controller
+        # In terms of true environment reward of your rolled out
+        # trajectory using the MPC controller
+        returns = list([ds['rewards'] for ds in data_sample])
         logz.log_tabular('AverageReturn', np.mean(returns))
         logz.log_tabular('StdReturn', np.std(returns))
         logz.log_tabular('MinimumReturn', np.min(returns))
         logz.log_tabular('MaximumReturn', np.max(returns))
 
         logz.dump_tabular()
+
 
 def main():
 
@@ -216,7 +267,8 @@ def main():
     # Make data directory if it does not already exist
     if not(os.path.exists('data')):
         os.makedirs('data')
-    logdir = args.exp_name + '_' + args.env_name + '_' + time.strftime("%d-%m-%Y_%H-%M-%S")
+    logdir = args.exp_name + '_' + args.env_name + '_' +\
+        time.strftime("%d-%m-%Y_%H-%M-%S")
     logdir = os.path.join('data', logdir)
     if not(os.path.exists(logdir)):
         os.makedirs(logdir)
@@ -225,24 +277,25 @@ def main():
     if args.env_name is "HalfCheetah-v1":
         env = HalfCheetahEnvNew()
         cost_fn = cheetah_cost_fn
-    train(env=env, 
-                 cost_fn=cost_fn,
-                 logdir=logdir,
-                 render=args.render,
-                 learning_rate=args.learning_rate,
-                 onpol_iters=args.onpol_iters,
-                 dynamics_iters=args.dyn_iters,
-                 batch_size=args.batch_size,
-                 num_paths_random=args.random_paths, 
-                 num_paths_onpol=args.onpol_paths, 
-                 num_simulated_paths=args.simulated_paths,
-                 env_horizon=args.ep_len, 
-                 mpc_horizon=args.mpc_horizon,
-                 n_layers = args.n_layers,
-                 size=args.size,
-                 activation=tf.nn.relu,
-                 output_activation=None,
-                 )
+    train(
+        env=env,
+        cost_fn=cost_fn,
+        logdir=logdir,
+        render=args.render,
+        learning_rate=args.learning_rate,
+        onpol_iters=args.onpol_iters,
+        dynamics_iters=args.dyn_iters,
+        batch_size=args.batch_size,
+        num_paths_random=args.random_paths,
+        num_paths_onpol=args.onpol_paths,
+        num_simulated_paths=args.simulated_paths,
+        env_horizon=args.ep_len,
+        mpc_horizon=args.mpc_horizon,
+        n_layers=args.n_layers,
+        size=args.size,
+        activation=tf.nn.relu,
+        output_activation=None)
+
 
 if __name__ == "__main__":
     main()
